74067c27ff21aceda3c76a6cdef1bfb2e9b0ceff
max <jmclarke51@gmail.com>
2019-01-15 22:08:10 -0600

    Decided to focus on per 36 min table during the season. Extracted that column and salaries column. The thought is that salaries will be the prediction, specifically, whether a player is over or under paid

--- basketball.py
+++ basketball.py
@@ -11,16 +11,16 @@ def get_tables(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
 
-    # tables are hiddne behond html comments. This parses that
+    # tables are hidden behind html comments. This parses that
     macaroni = soup.prettify().split('<!--')
     macaroni = [mac.strip('-->') for mac in macaroni]
 
     tables = []
 
-    for nood in macaroni:
-        udon = BeautifulSoup(nood, 'lxml').find_all('table')
-        if udon != []:
-            tables.append(udon[0])
+    for roi in macaroni:
+        table = BeautifulSoup(roi, 'lxml').find_all('table')
+        if table != []:
+            tables.append(table[0])
 
     dfs = []
 
@@ -43,10 +43,53 @@ def get_tables(url):
 
     return dfs
 
+
+# commenting out the below because I don't need every single table
+# I'll come to tables as I need them.
+# For now, I will focus on Per 36 Min table
+
+# def clean_tables(dfs):  # I'll run a for loop for each df in dfs
+#     '''
+#     Takes list of pandas dataframes for an NBA team in 1999 and cleans each
+#     DataFrame
+#     '''
+#     clean_dfs = []
+
+#     # clean df 0
+#     df0 = dfs[0]
+#     df0 = df0.apply(str.strip)  # strip white space from number
+#     clean_dfs.append(df0)
+
+#     # df 1 is already clean
+#     df1 = dfs[1]
+#     df1.columns = ['Name', 'Title']
+#     clean_dfs.append(df1)
+
+#     # clean df 2
+#     df2 = dfs[2]
+#     df2.columns = df2.iloc[0, :]
+#     df2.drop(0, inplace=True)
+#     df2.reset_index(inplace=True, drop=True)
+#     clean_dfs.append(df2)
+
+#     # clean df 3
+#     df3 = dfs[3]
+#     [3].drop(0, inplace=True)
+#     cols = df3.iloc[0, :]
+#     df3.columns = cols
+#     df3.drop(1, inplace=True)
+#     df3.reset_index(inplace=True)
+#     clean_dfs.append(df3)
+
+#     # clean df 4
+
+
+
+
 if __name__ == '__main__':
 
     # testing on just one team, one season
-    URL = 'https://www.basketball-reference.com/teams/SAS/2006.html'
+    URL = 'https://www.basketball-reference.com/teams/SAS/1999.html'
     dfs = get_tables(URL)
 
     print(dfs[9])
